b2384f1cf72952bde7e4d42a3359c2013552ede5
Jaeyoung-Lim <jalim@ethz.ch>
2022-09-22 01:13:06 +0200

    Fix tests

--- test/test_grid_map_geo.cpp
+++ test/test_grid_map_geo.cpp
@@ -1,4 +1,4 @@
-#include "grid_map_geo/grid_map_geo.h"
+#include "grid_map_geo/transform.h"
 
 #include <gtest/gtest.h>
 #include <iostream>
@@ -11,11 +11,11 @@ TEST(GridMapTest, geoTransform) {
   Eigen::Vector3d berghaus_wgs84(9.9219592, 46.7209147, 488.0);
 #endif
   Eigen::Vector3d berghaus_lv03(789823.96735451114, 177416.47911055354, 440.3752994351089);
-  Eigen::Vector3d tranformed_lv03 = GridMapGeo::transformCoordinates(ESPG::WGS84, ESPG::CH1903_LV03, berghaus_wgs84);
+  Eigen::Vector3d tranformed_lv03 = transformCoordinates(ESPG::WGS84, ESPG::CH1903_LV03, berghaus_wgs84);
   EXPECT_NEAR(tranformed_lv03(0), berghaus_lv03(0), 0.0001);
   EXPECT_NEAR(tranformed_lv03(1), berghaus_lv03(1), 0.0001);
   // EXPECT_NEAR(tranformed_lv03(2), berghaus_lv03(2), 0.0001);
-  Eigen::Vector3d tranformed_wgs84 = GridMapGeo::transformCoordinates(ESPG::CH1903_LV03, ESPG::WGS84, berghaus_lv03);
+  Eigen::Vector3d tranformed_wgs84 = transformCoordinates(ESPG::CH1903_LV03, ESPG::WGS84, berghaus_lv03);
   EXPECT_NEAR(tranformed_wgs84(0), berghaus_wgs84(0), 0.0001);
   EXPECT_NEAR(tranformed_wgs84(1), berghaus_wgs84(1), 0.0001);
   // EXPECT_NEAR(tranformed_wgs84(2), berghaus_wgs84(2), 0.0001);
@@ -29,7 +29,7 @@ TEST(GridMapTest, geoTransform) {
       "PARAMETER[\"rectified_grid_angle\", 90], PARAMETER[\"scale_factor\", 1], PARAMETER[\"false_easting\", 600000], "
       "PARAMETER[\"false_northing\", 200000], UNIT[\"metre\", 1, AUTHORITY[\"EPSG\", \"9001\"]], AXIS[\"Y\", EAST], "
       "AXIS[\"X\", NORTH], AUTHORITY[\"EPSG\", \"21781\"]]";
-  Eigen::Vector3d tranformed_lv032 = GridMapGeo::transformCoordinates(ESPG::WGS84, wkt, berghaus_wgs84);
+  Eigen::Vector3d tranformed_lv032 = transformCoordinates(ESPG::WGS84, wkt, berghaus_wgs84);
   EXPECT_NEAR(tranformed_lv032(0), berghaus_lv03(0), 0.0001);
   EXPECT_NEAR(tranformed_lv032(1), berghaus_lv03(1), 0.0001);
   // EXPECT_NEAR(tranformed_lv032(2), berghaus_lv03(2), 0.0001);
