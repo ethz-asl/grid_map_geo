5dd8a2a487a7b5444d3adc835f73fe379dd0ade9
Jaeyoung Lim <jalim@ethz.ch>
2022-09-30 16:11:43 +0200

    Add explicit inline transforms

--- include/grid_map_geo/transform.h
+++ include/grid_map_geo/transform.h
@@ -51,7 +51,7 @@ enum class ESPG { ECEF = 4978, WGS84 = 4326, WGS84_32N = 32632, CH1903_LV03 = 21
  * @param source_coordinates
  * @return Eigen::Vector3d
  */
-Eigen::Vector3d transformCoordinates(ESPG src_coord, ESPG tgt_coord, const Eigen::Vector3d source_coordinates) {
+inline Eigen::Vector3d transformCoordinates(ESPG src_coord, ESPG tgt_coord, const Eigen::Vector3d source_coordinates) {
   OGRSpatialReference source, target;
   source.importFromEPSG(static_cast<int>(src_coord));
   target.importFromEPSG(static_cast<int>(tgt_coord));
@@ -75,7 +75,7 @@ Eigen::Vector3d transformCoordinates(ESPG src_coord, ESPG tgt_coord, const Eigen
  * @param source_coordinates
  * @return Eigen::Vector3d
  */
-Eigen::Vector3d transformCoordinates(ESPG src_coord, const std::string wkt, const Eigen::Vector3d source_coordinates) {
+inline Eigen::Vector3d transformCoordinates(ESPG src_coord, const std::string wkt, const Eigen::Vector3d source_coordinates) {
   OGRSpatialReference source, target;
   char* wkt_string = const_cast<char*>(wkt.c_str());
   source.importFromEPSG(static_cast<int>(src_coord));
