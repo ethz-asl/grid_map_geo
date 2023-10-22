47746fa0e418cdf666e914293329d7a75154e226
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-10-21 19:25:40 -0500

    ros2: updates to cmake and launch files to support build on macOS
    Port to ROS 2
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>
    
    Get launch file running aside from resources
    
    * Update docs
    * Remove ROS1 cruft
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>
    
    - update gdal include path if __APPLE__
    - consolidate cmake files and install executable
    - update rviz to rviz2 in launch file
    - add arguments and rename python launch file
    - link directory workaround for yaml-cpp on aarch64.
    - update launch files to use correct param name for tif_color_path.
    - CMakeLists.txt improvements.
    - remove target_include_directories for test_tif_loader.
    - add dependency on yaml_cpp_vendor.
    - restore namespace to tif_loader node in launch file.
    - exclude executable from target exports and fix header install directory.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>
    
    Co-authored-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>
    Co-authored-by: Rhys Mainwaring <rhys.mainwaring@me.com>

---
+++ launch/load_tif.launch.py
@@ -0,0 +1,84 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """Generate a launch description."""
+
+    pkg_grid_map_geo = get_package_share_directory("grid_map_geo")
+
+    # static transform node
+    static_transform_publisher = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="world_map",
+        arguments=[
+            "--frame-id",
+            "world",
+            "--child-frame-id",
+            "map",
+        ],
+    )
+
+    # tif loader node
+    tif_loader = Node(
+        package="grid_map_geo",
+        namespace="grid_map_geo",
+        executable="test_tif_loader",
+        name="tif_loader",
+        parameters=[
+            {"tif_path": LaunchConfiguration("tif_path")},
+            {"tif_color_path": LaunchConfiguration("tif_color_path")},
+        ],
+        output="screen",
+        emulate_tty=True,
+    )
+
+    # rviz node
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(pkg_grid_map_geo, "launch", "config.rviz")],
+        condition=IfCondition(LaunchConfiguration("rviz")),
+    )
+
+    default_location = "sargans"
+    default_tif_file = "sargans.tif"
+    default_tif_color_file = "sargans_color.tif"
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "rviz", default_value="true", description="Open RViz."
+            ),
+            DeclareLaunchArgument(
+                "location",
+                default_value=default_location,
+                description="Location.",
+            ),
+            DeclareLaunchArgument(
+                "tif_path",
+                default_value=os.path.join(
+                    pkg_grid_map_geo, "resources", default_tif_file
+                ),
+                description="Full path to the elevation map file.",
+            ),
+            DeclareLaunchArgument(
+                "tif_color_path",
+                default_value=os.path.join(
+                    pkg_grid_map_geo, "resources", default_tif_color_file
+                ),
+                description="Full path to the elevation texture file.",
+            ),
+            static_transform_publisher,
+            tif_loader,
+            rviz,
+        ]
+    )
--- src/test_tif_loader.cpp
+++ src/test_tif_loader.cpp
@@ -55,6 +55,9 @@ class MapPublisher : public rclcpp::Node {
       std::string file_path = this->declare_parameter("tif_path", ".");
       std::string color_path = this->declare_parameter("tif_color_path", ".");
 
+      RCLCPP_INFO_STREAM(get_logger(), "file_path " << file_path);
+      RCLCPP_INFO_STREAM(get_logger(), "color_path " << color_path);
+
       map_ = std::make_shared<GridMapGeo>();
       map_->Load(file_path, false, color_path);
       timer_ = this->create_wall_timer(5s, std::bind(&MapPublisher::timer_callback, this));
