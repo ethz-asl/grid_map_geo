3663f3798623a144d68a54aa019fdbdfc24a4705
Jaeyoung-Lim <Jaeyoung-Lim@users.noreply.github.com>
2024-01-18 02:59:10 +0000

    deploy: 8f7b3766f763c87cc340fba944b962547569d67e

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['getglobalorigin_0',['getGlobalOrigin',['../classGridMapGeo.html#adc977cd9c5d58b18301e3d11c416cf4a',1,'GridMapGeo']]],
-  ['getgridmap_1',['getGridMap',['../classGridMapGeo.html#a9fd61121682987e2b951e589a63f56c8',1,'GridMapGeo']]],
-  ['grid_5fmap_5fgeo_2',['grid_map_geo',['../index.html',1,'']]],
-  ['gridmapgeo_3',['GridMapGeo',['../classGridMapGeo.html',1,'GridMapGeo'],['../classGridMapGeo.html#a55981401f0ba6681ed5179c3ffaa2a8d',1,'GridMapGeo::GridMapGeo()']]]
+  ['getcoordinatename_0',['getCoordinateName',['../classGridMapGeo.html#ae77a5f02d46e9adc11b2a1507571f1b0',1,'GridMapGeo']]],
+  ['getglobalorigin_1',['getGlobalOrigin',['../classGridMapGeo.html#adc977cd9c5d58b18301e3d11c416cf4a',1,'GridMapGeo']]],
+  ['getgridmap_2',['getGridMap',['../classGridMapGeo.html#a9fd61121682987e2b951e589a63f56c8',1,'GridMapGeo']]],
+  ['grid_5fmap_5fgeo_3',['grid_map_geo',['../index.html',1,'']]],
+  ['gridmapgeo_4',['GridMapGeo',['../classGridMapGeo.html',1,'GridMapGeo'],['../classGridMapGeo.html#a55981401f0ba6681ed5179c3ffaa2a8d',1,'GridMapGeo::GridMapGeo()']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['getglobalorigin_0',['getGlobalOrigin',['../classGridMapGeo.html#adc977cd9c5d58b18301e3d11c416cf4a',1,'GridMapGeo']]],
-  ['getgridmap_1',['getGridMap',['../classGridMapGeo.html#a9fd61121682987e2b951e589a63f56c8',1,'GridMapGeo']]],
-  ['gridmapgeo_2',['GridMapGeo',['../classGridMapGeo.html#a55981401f0ba6681ed5179c3ffaa2a8d',1,'GridMapGeo']]]
+  ['getcoordinatename_0',['getCoordinateName',['../classGridMapGeo.html#ae77a5f02d46e9adc11b2a1507571f1b0',1,'GridMapGeo']]],
+  ['getglobalorigin_1',['getGlobalOrigin',['../classGridMapGeo.html#adc977cd9c5d58b18301e3d11c416cf4a',1,'GridMapGeo']]],
+  ['getgridmap_2',['getGridMap',['../classGridMapGeo.html#a9fd61121682987e2b951e589a63f56c8',1,'GridMapGeo']]],
+  ['gridmapgeo_3',['GridMapGeo',['../classGridMapGeo.html#a55981401f0ba6681ed5179c3ffaa2a8d',1,'GridMapGeo']]]
 ];
